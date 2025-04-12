6efa07099c92a270b03dc173461efcb7526d6766
Simone <skixmix@gmail.com>
2025-04-12 14:06:29 +0200

    fix: fixed typeorm init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,31 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HealthCheckModule } from './health-check/health-check.module';
 
-const databaseConnectionModule = TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    autoLoadEntities: true,
-    synchronize: false,
+const databaseConnectionModule = TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: false,
+    }),
 });
 
 @Module({
-    imports: [databaseConnectionModule, ConfigModule.forRoot(), HealthCheckModule],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        databaseConnectionModule,
+        HealthCheckModule,
+    ],
     controllers: [],
     providers: [],
 })
